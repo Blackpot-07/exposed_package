278902b313cb27466241eb45a529f5c5e7faa327
Abdullah Irfan <104737181+Blackpot-07@users.noreply.github.com>
2025-07-16 20:17:19 +0500

    Update setup_code.py

--- SetupPackage/setup_code.py
+++ SetupPackage/setup_code.py
@@ -1,7 +1,7 @@
 def hello(notebook_id : str):
-  if notebook_id = '1':
+  if notebook_id == '1':
     print('hello world')
-  elif notebook_id = '2':
+  elif notebook_id == '2':
     print('hello world2')
   else:
     print('incorrect notebook id')
