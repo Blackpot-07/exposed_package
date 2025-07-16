e0a5fc11237f8433195a9e71d95593f61807ab21
Abdullah Irfan <104737181+Blackpot-07@users.noreply.github.com>
2025-07-16 19:48:21 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
-def hello():
-  print('hello world')
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Abdullah',
+    author_email='iabdullah416@gmail.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Blackpot-07/exposed_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
